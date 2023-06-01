24f99ffdb1571842ef4510e3b688098e61be87f6
miyamusaxi <277353.git.soft@gmail.com>
2023-06-01 10:03:37 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# pandas_plan_category
---
+++ src/analise.py
@@ -0,0 +1,50 @@
+import pandas as pd
+#import plotly.express as px
+
+def criar_dicionario(lista, depart):
+    lista2 =[]
+    for categoria, grupo, departamento in lista:
+        if departamento == depart:
+            lista2.append(categoria)
+            lista2.append(grupo)
+    dic = {depart: lista2}
+    return dic
+
+
+if __name__ == '__main__':
+    lista_dicionarios=[]    
+    df = pd.read_csv(r"C:\export_produtos\produtos_proxpect_.csv", encoding='utf-8')
+    lista = df.values.tolist()
+    df = df.drop(columns=['categoria','grupo'])
+    df = df.drop_duplicates()
+    lista_depart = df.values.tolist()
+    # print(len(lista_depart))
+    for item in lista_depart:
+        x =criar_dicionario(lista=lista, depart=item)
+        lista_dicionarios.append(x) 
+    print(lista_dicionarios)
+# lista = df.values.tolist()
+
+# for i in lista:
+#     total, produtos_total, data, semana = i
+#     if semana == 1:
+#         tot_1 = tot_1 + total
+#     elif semana == 2:
+#         tot_2 = tot_2 + total        
+
+# df = pd.read_csv(r"C:\export_produtos\analise_abrasivos_.csv", encoding='latin-1')
+# df = df.drop(columns= 'id.1')
+# lista = df.values.tolist()
+
+# lista_ = []
+
+# for i in lista:
+#     id_, emp, cnpj, data, total, produto = i
+#     if 'GRANALHA' not in produto:
+#         dicionario = {'empresa':emp, 'cnpj':cnpj, 'data':data, 'total_venda':total, 'produto':produto}
+#         lista_.append(dicionario)
+
+# data_frame = pd.DataFrame(lista_)
+# data_frame.to_csv(r"C:\export_produtos\analise_abrasivos.csv", index=False, encoding='latin-1')
+
+# #print(df.head())
