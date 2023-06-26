bcb6af8c3aff43aa7e26ade92fdf504b2aa75923
miyamusaxi <277353.git.soft@gmail.com>
2023-06-26 10:27:18 -0300

    Atualização do Código Categorização

--- src/analise.py
+++ src/analise.py
@@ -1,60 +1,121 @@
 import pandas as pd
-#import plotly.express as px
+import numpy as np
+from datetime import datetime
 
-def criar_dicionario(lista, depart):
-    lista2 =[]
-    for categoria, grupo, departamento in lista:
-        # print(f'{categoria} - {grupo} - {departamento}')
-        # breakpoint()
-        if departamento == depart:
-            lista2.append(categoria)
-            lista2.append(grupo)
-    dic = {depart: lista2}
+def tratar_key(key):
+    key = str(key).strip('dict_keys')
+    key = key.strip("[(')]")
+    return key
+
+def criar_dicionario(lista, cat):
+    lista2 = []
+    for produto, categoria in lista:
+        if categoria == cat:
+            lista2.append(produto)
+    dic = {cat: lista2}
     return dic
 
-def tratar_item_lista(string, remover):
+
+def tratar_str(string, remover):
     string = str(string)
     string = string.strip(remover)
-    string = string.strip() 
-    string = string.replace("'","")    
+    string = string.strip()
+    string = string.replace("'", "")
     return string
+
+def categorizar(row, prod, key,conteudo,prod2):
+    if conteudo and prod.lower() in key.lower():
+        conteudo = [str(conte).lower() for conte in conteudo]
+        if 'serra' in prod2 and 'moto' not in prod2:
+            return pd.DataFrame({
+                    'id' :row['id'],
+                    'codigo interno': row['Código interno'],
+                    'produto':row['Titulo'],
+                    'preco':row['Preço'],
+                    'categoria':'Serra',
+                    'ativo':row['Ativo']
+                    },index=[0])
+        elif ((prod.lower()in conte) for conte in conteudo) or ((prod2.lower() in conte) for conte in conteudo):
+            # MONTA UM DICIONARIO COM A NOVA CATEGORIA E ADICIONA NUMA LISTA DE DICIONARIOS
+            return pd.DataFrame({
+                    'id' :row['id'],
+                    'codigo interno': row['Código interno'],
+                    'produto':row['Titulo'],
+                    'preco':row['Preço'],
+                    'categoria':key,
+                    'ativo':row['Ativo']
+                    },index=[0])
+    elif not prod.lower() in key.lower():
+        return pd.DataFrame({
+                    'id' :row['id'],
+                    'codigo interno': row['Código interno'],
+                    'produto':row['Titulo'],
+                    'preco':row['Preço'],
+                    'categoria':CAT_CORINGA,
+                    'ativo':row['Ativo']
+                    },index=[0])
+
+
 
 
 if __name__ == '__main__':
-    lista_dicionarios=[]    
-    df = pd.read_csv(r"C:\export_produtos\produtos_proxpect_.csv", encoding='utf-8')
+    CAT_CORINGA = 'Cama, Mesa e Banho'
+    listadics = []
+    listaitens = []
+    cont = 0
+
+    df = pd.read_csv(
+        r"C:\export_produtos\categorias.csv", encoding='utf-8')
+    tipo = type(df)
+    df = df.drop(columns=['departamento', 'grupo'])
     lista = df.values.tolist()
-    df = df.drop(columns=['categoria','grupo'])
+
+    df = df.drop(columns=['titulo'])
+
     df = df.drop_duplicates()
-    lista_depart = df.values.tolist()
-    # print(len(lista_depart))
-    for i in lista_depart:
-        depart = tratar_item_lista(i,'[]')
-        x =criar_dicionario(lista=lista, depart=depart)
-        lista_dicionarios.append(x) 
-    print(lista_dicionarios)
-# lista = df.values.tolist()
 
-# for i in lista:
-#     total, produtos_total, data, semana = i
-#     if semana == 1:
-#         tot_1 = tot_1 + total
-#     elif semana == 2:
-#         tot_2 = tot_2 + total        
+    listacat = df.values.tolist()
 
-# df = pd.read_csv(r"C:\export_produtos\analise_abrasivos_.csv", encoding='latin-1')
-# df = df.drop(columns= 'id.1')
-# lista = df.values.tolist()
+    for i in listacat:
+        cat = tratar_str(str(i),'['']')
+        cat = cat.strip()
+        cat = cat.replace('"','')
+        x = criar_dicionario(lista=lista, cat=cat)
+        listaitens.append(x)
 
-# lista_ = []
+    df = pd.read_excel(
+        r"C:\py_projects\data_analise\doc\Categorias J2D DISTRIBUIDORA.xlsx").replace({np.nan: None}).to_dict(orient='records')
 
-# for i in lista:
-#     id_, emp, cnpj, data, total, produto = i
-#     if 'GRANALHA' not in produto:
-#         dicionario = {'empresa':emp, 'cnpj':cnpj, 'data':data, 'total_venda':total, 'produto':produto}
-#         lista_.append(dicionario)
+    cats = [tratar_key(str(key.keys())) for key in listaitens]
+    conteudo = [dic.get(cats[i]) for i, dic in enumerate(listaitens)]
+    for i, row in enumerate(df):
+        if row['Ativo'] == 'Sim' and row['Nova Categoria'] == None:
+        #TRATAMENTO DE STRING
+            newstring = row['Titulo'].split('-')
+            prod = newstring[0].split()
+            prod = [str(produto).lower() for produto in prod]
+            prodComp = ' '.join(prod)
+            produto = prod[0]    
+            for i, key in enumerate(cats):
+                temp = (categorizar(row,produto,key,conteudo[i],prodComp))
+                if type(temp) == tipo :
+                    listadics.append(temp)
+                    break
+    #exportando pra planilha
+    if listadics:
+        now = datetime.now()
+        result = pd.concat(listadics)
+        result.to_excel(f'C:\py_projects\data_analise\doc\categorizado-{now:%d%m%y-%H%M%S}.xlsx')
+        print('success')
+    # else:
+    #     print('DEU ERRO CE É BURRAO EIN')
 
-# data_frame = pd.DataFrame(lista_)
-# data_frame.to_csv(r"C:\export_produtos\analise_abrasivos.csv", index=False, encoding='latin-1')
-
-# #print(df.head())
+    # df = pd.read_excel(
+    #     r'C:\py_projects\data_analise\doc\teste.xlsx').replace({np.nan: None}).to_dict(orient='records')
+    # for col in df:
+    #     stringstrip = col['descricao'].strip()
+    #     newstring = ''.join([i for i in stringstrip if not i.isdigit()])
+        
+    #     if len(prod) > 4:
+    #         if prod.lower() in col['categoria'].lower():
+    #             print(f'{col["descricao"]} - {col["categoria"]}')
